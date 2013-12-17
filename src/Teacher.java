fcc67de0c8861c7a94446abc80f89fe925a8f83f
Rutger van Willigen <rutgervanwilligen@gmail.com>
2013-12-17 16:33:03 +0100

    jaaaaaaaaaa teacher ook nog grmbl

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
 
 public class Teacher {
+
+	public static final String FIRST_GRADE = "first grade teacher";
+	public static final String SECOND_GRADE = "second grade teacher";
 
 	protected String name;
 	protected String code;
 	protected int weeklyTeachingHours;
+	protected String grade;
+	protected ArrayList<Subject> subjects;
+	
+	
 	
 }
