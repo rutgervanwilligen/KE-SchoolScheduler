02ffcc6dafb6c5caeb6561332dbf77d51c4fcf78
Rutger van Willigen <rutgervanwilligen@gmail.com>
2013-12-19 14:31:39 +0100

    niet het noemen waard

--- src/ClassInSchool.java
+++ src/ClassInSchool.java
@@ -9,6 +9,13 @@ public abstract class ClassInSchool {
 	protected char letter;
 	protected Level level;
 	
+	/**
+	 * Create new class in school.
+	 * 
+	 * @param year int Class's year
+	 * @param letter char Class's letter
+	 * @param level Level Class's education level
+	 */
 	public ClassInSchool (int year, char letter, Level level) {
 		this.year = year;
 		this.letter = letter;
@@ -16,14 +23,25 @@ public abstract class ClassInSchool {
 		name = level + Integer.toString(year) + letter;
 	}
 	
+	/**
+	 * Add student to the class.
+	 * 
+	 * @param student Student Student to add
+	 */	
 	public boolean addStudent(Student student) {
 		return studentList.add(student);
 	}
 
+	/**
+	 * Returns the class's name.
+	 */	
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Returns the class's education level.
+	 */	
 	public Level getLevel() {
 		return level;
 	}
